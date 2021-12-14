549c07d6b4912231c124b958808e8df799e4f867
alonecandies <dragonfrezze@gmail.com>
2021-12-15 03:23:10 +0700

    first submit

---
+++ assets/js/aos.js
@@ -0,0 +1,6 @@
+AOS.init({
+    easing:"slide",
+    once:true,
+    duration:1000,
+    delay:0
+});
---
+++ assets/js/navToggle.js
@@ -0,0 +1,4 @@
+function navbarToggle() {
+    var x=document.getElementsByClassName("navigation-menu")[0];
+    x.classList.toggle("navigation-menu__responsive")
+}
